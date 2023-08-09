9667bf83147338d5d1d80a91fc5ffd726de2a85f
Peter Prescott <prescott.peter@gmail.com>
2023-08-09 20:19:25 +0100

    Add i3 config and screenlayout scripts

---
+++ .screenlayout/connect.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+xrandr --setprovideroutputsource 4 0
+xrandr --setprovideroutputsource 3 0
+xrandr --setprovideroutputsource 2 0
+xrandr --setprovideroutputsource 1 0
+
---
+++ .screenlayout/display.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-1 --primary --mode 1600x900 --pos 0x0 --rotate right --output DP-1 --mode 1440x900 --pos 2820x0 --rotate left --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --mode 1920x1080 --pos 900x180 --rotate normal
