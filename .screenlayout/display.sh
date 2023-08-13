0926bc6dd1b49a0ad9975fc3e39ad42fa1bbb23e
Peter Prescott <prescott.peter@gmail.com>
2023-08-13 19:36:10 +0100

    Change workspace display settings

--- .screenlayout/display.sh
+++ .screenlayout/display.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VGA-1 --primary --mode 1600x900 --pos 0x0 --rotate right --output DP-1 --mode 1440x900 --pos 2820x0 --rotate left --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --mode 1920x1080 --pos 900x180 --rotate normal
+xrandr --output eDP-1 --off --output DP-1 --primary --mode 1920x1080 --pos 900x520 --rotate normal --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-2-1 --mode 1440x900 --pos 2820x160 --rotate left --output DP-2-2 --mode 1600x900 --pos 0x0 --rotate left --output DP-2-3 --off
