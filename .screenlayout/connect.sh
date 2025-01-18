a79e62f48fb72059e58d00e675b3fa4fb5d02ba9
Peter Prescott <prescott.peter@gmail.com>
2025-01-18 00:51:02 +0000

    Update screen setup

--- .screenlayout/connect.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-xrandr --setprovideroutputsource 4 0
-xrandr --setprovideroutputsource 3 0
-xrandr --setprovideroutputsource 2 0
-xrandr --setprovideroutputsource 1 0
-
--- .screenlayout/display.sh
+++ .screenlayout/display.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output DP-1 --primary --mode 1920x1080 --pos 900x520 --rotate normal --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-2-1 --mode 1440x900 --pos 2820x160 --rotate left --output DP-2-2 --mode 1600x900 --pos 0x0 --rotate left --output DP-2-3 --off
+xrandr --output DVI-I-1-1 --primary --mode 1920x1080 --pos 900x180 --rotate normal --output DP-2 --mode 1440x900 --pos 2820x0 --rotate left --output DP-1 --mode 1600x900 --pos 0x0 --rotate left 
